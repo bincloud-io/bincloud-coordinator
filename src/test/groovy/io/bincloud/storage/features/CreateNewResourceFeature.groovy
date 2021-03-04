f73b84c8b96843abe09d22b38389401d562bfdf1
Dmitry Mikhaylenko <dmitry_mikhaylenko@epam.com>
2021-03-04 12:43:50 +0400

    [milestone/#1][issue/#1] Fix scenarios

--- src/test/groovy/io/bincloud/storage/features/CreateNewResourceFeature.groovy
+++ src/test/groovy/io/bincloud/storage/features/CreateNewResourceFeature.groovy
@@ -8,29 +8,21 @@ import spock.lang.Specification
 	As a user, I want to create a resource that will represent the corresponding file in the storage
 """)
 class CreateNewResourceFeature extends Specification {
-	def "Scenario: create resource which doesn't exist and input command is valid"() {
-		given: "Resource doesn't exist in repository"
-		and: "The valid input command"
-		when: "The create new resource command executed"
-		then: "The resource has been saved in the store"
-		and: "The saved resource id the same as generated id"
-		and: "The saved resource name the same as in command"
+	def "Scenario: create resource new resource"() {
+		given: "Resource hasn't been created yet"
+		and: "The request parameters are valid"
+		when: "The create new resource operation has been called"
+		then: "The new resource has been created in the store"
+		and: "The generated identifier has been assigned to resource"
+		and: "The "
 			1==2
 	}
 	
-	def "Scenario: create resource which doesn't exist but input command is invalid"() {
-		given: "Resource doesn't exist in repository"
-		and: "The invalid input command"
-		when: "The create new resource command executed"
-		then: "The error occurred notifying about invalid input command"
-			1==2
-	}
-	
-	def "Scenario: create resource which already exists but command is valid"() {
-		given: "Resource exists in repository"
-		and: "The valid input command"
-		when: "The create new resource command executed"
-		then: "The error occurred notifying about existing resource"
+	def "Scenario: try to create resource with invalid request parameters"() {
+		given: "Resource hasn't been created yet"
+		and: "The resource creation command is invalid"
+		when: "The create new resource operation has been called"
+		then: "The validation error occurred"
 			1==2
 	}
 }
