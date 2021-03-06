4b73c742ea647a207e3f5b878e781fb59aab566a
Dmitry Mikhaylenko <dmitry_mikhaylenko@epam.com>
2021-03-06 10:28:39 +0400

    [milestone/#1] [issue/#1] add feature test and resource service skeleton

---
+++ src/main/java/io/bincloud/storage/application/CreateResourceService.java
@@ -0,0 +1,18 @@
+package io.bincloud.storage.application;
+
+import io.bincloud.common.ValidationService;
+import io.bincloud.storage.domain.model.resource.Resource.IdGenerator;
+import io.bincloud.storage.domain.model.resource.Resource.ResourceDetails;
+import io.bincloud.storage.domain.model.resource.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class CreateResourceService {
+	private final IdGenerator idGenerator;
+	private final ValidationService validationService;
+	private final ResourceRepository resourceRepository;
+	
+	public Long createNewResource(ResourceDetails resourceDetails) {
+		throw new UnsupportedOperationException();
+	}
+}
--- src/test/groovy/io/bincloud/storage/features/CreateNewResourceFeature.groovy
+++ src/test/groovy/io/bincloud/storage/features/CreateNewResourceFeature.groovy
@@ -1,5 +1,14 @@
 package io.bincloud.storage.features
 
+import io.bincloud.common.ValidationException
+import io.bincloud.common.ValidationService
+import io.bincloud.common.ValidationState
+import io.bincloud.common.ApplicationException.Severity
+import io.bincloud.storage.application.CreateResourceService
+import io.bincloud.storage.domain.model.resource.Resource
+import io.bincloud.storage.domain.model.resource.ResourceRepository
+import io.bincloud.storage.domain.model.resource.Resource.IdGenerator
+import io.bincloud.storage.domain.model.resource.Resource.ResourceDetails
 import spock.lang.Narrative
 import spock.lang.Specification
 
@@ -8,21 +17,66 @@ import spock.lang.Specification
 	As a user, I want to create a resource that will represent the corresponding file in the storage
 """)
 class CreateNewResourceFeature extends Specification {
-	def "Scenario: create resource new resource"() {
-		given: "Resource hasn't been created yet"
-		and: "The request parameters are valid"
-		when: "The create new resource operation has been called"
-		then: "The new resource has been created in the store"
-		and: "The generated identifier has been assigned to resource"
-		and: "The "
-			1==2
-	}
+	private IdGenerator resourceIdGenerator;
+	private ValidationService validationService;
+	private ResourceRepository resourceRepository;
+	private CreateResourceService serviceUnderTest;
 	
-	def "Scenario: try to create resource with invalid request parameters"() {
-		given: "Resource hasn't been created yet"
-		and: "The resource creation command is invalid"
+	def setup() {
+		this.resourceIdGenerator = Stub(IdGenerator)
+		this.validationService = Stub(ValidationService)
+		this.resourceRepository = Mock(ResourceRepository)
+		this.serviceUnderTest = new CreateResourceService(
+			this.resourceIdGenerator,
+			this.validationService,
+			this.resourceRepository)
+	}
+
+	def "Scenario: create resource new resource"() {
+		given: "The resource id generator will generate id=1"
+		resourceIdGenerator.generateId() >> 1L
+
+		and: "The request parameters are valid"
+		ResourceDetails resourceDetails = Stub(ResourceDetails) {
+			getFileName() >> "file_name"
+		}
+		validationService.validate(resourceDetails) >> new ValidationState()
+
 		when: "The create new resource operation has been called"
+		Long createdResourceId = this.serviceUnderTest.createNewResource(resourceDetails)
+		
+		then: "The new resource has been stored"
+		Resource storedResource;
+		1 *  resourceRepository.save(_) >> {arguments -> storedResource = arguments[0]}
+		
+		and: "The stored resource id is returned"
+		createdResourceId == storedResource.id
+		
+		and: "The stored resource file name the same like in request parameters"
+		storedResource.fileName == "file_name"
+	}
+
+	def "Scenario: try to create resource with invalid request parameters"() {
+		given: "The resource id generator will generate id=1"
+		resourceIdGenerator.generateId() >> 1L
+		
+		and: "The resource creation command is invalid"
+		ResourceDetails resourceDetails = Stub(ResourceDetails) {
+			getFileName() >> "file_name"
+		}
+		validationService.validate(resourceDetails) >> new ValidationState()
+			.withGrouped("fileName", "error")
+			
+		when: "The create new resource operation has been called"
+		this.serviceUnderTest.createNewResource(resourceDetails)
+		
 		then: "The validation error occurred"
-			1==2
+		ValidationException error = thrown()
+		
+		and: "The validation error contains the same error like validation state"
+		error.errorState.groupedErrors.get("fileName") == Arrays.asList("error")
+		
+		and: "The validation error severity is business"
+		error.severity == Severity.BUSINESS
 	}
 }
