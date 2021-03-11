413f34cd09162c6fb97e1d153d8bfd94575a46a5
Dmitry Mikhaylenko <dmitry_mikhaylenko@epam.com>
2021-03-11 23:46:14 +0400

    Remove exception duplicate

--- src/main/java/io/bincloud/storage/application/resource/ResourceDoesNotExistException.java
+++ src/main/java/io/bincloud/storage/application/resource/ResourceDoesNotExistException.java
@@ -6,7 +6,7 @@ import lombok.Getter;
 @Getter
 public class ResourceDoesNotExistException extends ResourceManagementException {
 	private static final long serialVersionUID = -7373747956887477836L;
-	public static final Long ERROR_CODE = 3L;
+	public static final Long ERROR_CODE = 2L;
 	
 	public ResourceDoesNotExistException() {
 		super(Severity.BUSINESS, ERROR_CODE, "Resource does not exist.");
