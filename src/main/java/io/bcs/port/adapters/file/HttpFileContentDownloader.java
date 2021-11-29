8ec68bad05cff7d0cec2344c9f5e18d92758e02e
Dmitry Mikhaylenko <dmikhaylenko@hotmail.com>
2021-11-29 12:46:14 +0400

    Add rechargeable source

--- src/main/java/io/bcs/port/adapters/file/HttpFileContentDownloader.java
+++ src/main/java/io/bcs/port/adapters/file/HttpFileContentDownloader.java
@@ -1,11 +1,27 @@
 package io.bcs.port.adapters.file;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import io.bce.interaction.streaming.Destination;
+import io.bce.interaction.streaming.Streamer;
+import io.bce.interaction.streaming.binary.BinaryChunk;
 import io.bce.promises.Promise;
 import io.bcs.domain.model.file.ContentDownloader;
 import io.bcs.domain.model.file.FileContent;
+import io.bcs.port.adapters.common.OutputStreamDestination;
 
 public class HttpFileContentDownloader implements ContentDownloader {
-   
+    private final Streamer streamer;
+    private final Destination<BinaryChunk> destination;
+    
+    public HttpFileContentDownloader(Streamer streamer, HttpServletResponse servletResponse) throws IOException {
+        super();
+        this.destination = new OutputStreamDestination(servletResponse.getOutputStream());
+        this.streamer = streamer;
+    }
+    
     @Override
     public Promise<Void> downloadFullContent(FileContent content) {
         throw new UnsupportedOperationException();
@@ -20,5 +36,4 @@ public class HttpFileContentDownloader implements ContentDownloader {
     public Promise<Void> downloadContentRanges(FileContent content) {
         throw new UnsupportedOperationException();
     }
-
 }
