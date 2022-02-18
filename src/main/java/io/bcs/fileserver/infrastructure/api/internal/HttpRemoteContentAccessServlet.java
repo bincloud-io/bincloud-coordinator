1bd501426d1873c5f23ab655e86e717d0474378c
Dmitry Mikhaylenko <dmikhaylenko@hotmail.com>
2022-02-18 23:39:43 +0400

    Add internal content transfer API

--- src/main/java/io/bcs/fileserver/infrastructure/api/internal/HttpRemoteContentAccessServlet.java
+++ src/main/java/io/bcs/fileserver/infrastructure/api/internal/HttpRemoteContentAccessServlet.java
@@ -1,5 +1,6 @@
 package io.bcs.fileserver.infrastructure.api.internal;
 
+import com.github.jknack.handlebars.internal.Throwing.Supplier;
 import io.bce.domain.errors.ApplicationException;
 import io.bce.domain.errors.UnexpectedErrorException;
 import io.bce.interaction.streaming.Destination;
@@ -7,17 +8,24 @@ import io.bce.interaction.streaming.Source;
 import io.bce.interaction.streaming.Stream.Stat;
 import io.bce.interaction.streaming.Streamer;
 import io.bce.interaction.streaming.binary.BinaryChunk;
+import io.bce.interaction.streaming.binary.InputStreamSource;
 import io.bce.interaction.streaming.binary.OutputStreamDestination;
+import io.bce.promises.Deferred;
+import io.bce.promises.Deferred.DeferredFunction;
+import io.bce.promises.Promise;
 import io.bce.promises.Promises;
 import io.bcs.fileserver.domain.model.storage.ContentFragment;
 import io.bcs.fileserver.domain.model.storage.ContentLocator;
 import io.bcs.fileserver.domain.model.storage.DefaultContentLocator;
 import io.bcs.fileserver.domain.model.storage.FileStorage;
+import io.bcs.fileserver.infrastructure.FileServerConfigurationProperties;
 import io.bcs.fileserver.infrastructure.api.HttpAsyncExecutor;
 import io.bcs.fileserver.infrastructure.api.HttpResponseContext;
 import java.io.IOException;
 import javax.inject.Inject;
+import javax.servlet.AsyncContext;
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -43,34 +51,70 @@ public class HttpRemoteContentAccessServlet extends HttpServlet {
   @Inject
   private FileStorage fileStorage;
 
+  private FileServerConfigurationProperties fileServerConfigurationProperties;
+
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response)
       throws ServletException, IOException {
     HttpAsyncExecutor.of(request, response).executeAsynchronously(asyncContext -> {
-      Promises.<Stat>of(deferred -> {
-        HttpGetContentAccessCommand getAccessCommand = new HttpGetContentAccessCommand(request);
-        Source<BinaryChunk> source = getSource(getAccessCommand);
-        Destination<BinaryChunk> destination = getDestination(response);
+      transferContent(asyncContext, deferred -> {
+        HttpReadContentCommand getAccessCommand = new HttpReadContentCommand(request);
+        Source<BinaryChunk> source = getReadSource(getAccessCommand);
+        Destination<BinaryChunk> destination = getReadDestination(response);
         streamer.createStream(source, destination).start().delegate(deferred);
-      }).then(statistic -> {
-        HttpResponseContext.of(response).writeSuccessContext();
-      }).error(ApplicationException.class, error -> {
-        HttpResponseContext.of(response).writeErrorContext(error,
-            HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-      }).finalize(() -> asyncContext.complete());
+      });
     });
   }
 
-  private Source<BinaryChunk> getSource(HttpGetContentAccessCommand getAccessCommand) {
-    return fileStorage.getAccessOnRead(getAccessCommand.getContentLocator(),
-        getAccessCommand.getFragment());
+  @Override
+  protected void doPut(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    HttpAsyncExecutor.of(request, response).executeAsynchronously(asyncContext -> {
+      transferContent(asyncContext, deferred -> {
+        HttpWriteContentCommand writeCommand = new HttpWriteContentCommand(request);
+        Source<BinaryChunk> source = getWriteSource(request);
+        Destination<BinaryChunk> destination = getWriteDestination(writeCommand);
+        streamer.createStream(source, destination).start().delegate(deferred);
+      });
+    });
   }
 
-  private Destination<BinaryChunk> getDestination(HttpServletResponse response) {
-    return new OutputStreamDestination(getRequestOutputStream(response));
+  private void transferContent(AsyncContext context, DeferredFunction<Stat> deferred) {
+    HttpServletResponse response = (HttpServletResponse) context.getResponse();
+    Promises.<Stat>of(deferred).then(statistic -> {
+      HttpResponseContext.of(response).writeSuccessContext();
+    }).error(ApplicationException.class, error -> {
+      HttpResponseContext.of(response).writeErrorContext(error,
+          HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+    }).finalize(() -> context.complete());
   }
 
-  private ServletOutputStream getRequestOutputStream(HttpServletResponse response) {
+  private Source<BinaryChunk> getReadSource(HttpReadContentCommand readCommand) {
+    return fileStorage.getAccessOnRead(readCommand.getContentLocator(), readCommand.getFragment());
+  }
+
+  private Destination<BinaryChunk> getReadDestination(HttpServletResponse response) {
+    return new OutputStreamDestination(getResponseOutputStream(response));
+  }
+
+  private Source<BinaryChunk> getWriteSource(HttpServletRequest request) {
+    return new InputStreamSource(getRequestInputStream(request),
+        fileServerConfigurationProperties.getBufferSize());
+  }
+
+  private Destination<BinaryChunk> getWriteDestination(HttpWriteContentCommand writeCommand) {
+    return fileStorage.getAccessOnWrite(writeCommand.getContextLocator());
+  }
+
+  private ServletInputStream getRequestInputStream(HttpServletRequest request) {
+    try {
+      return request.getInputStream();
+    } catch (Exception error) {
+      throw new UnexpectedErrorException(error);
+    }
+  }
+
+  private ServletOutputStream getResponseOutputStream(HttpServletResponse response) {
     try {
       return response.getOutputStream();
     } catch (IOException error) {
@@ -79,33 +123,25 @@ public class HttpRemoteContentAccessServlet extends HttpServlet {
   }
 
   @RequiredArgsConstructor
-  private static class HttpGetContentAccessCommand {
+  private static class HttpReadContentCommand {
     private final HttpServletRequest request;
 
     public ContentLocator getContentLocator() {
-      return new DefaultContentLocator(getStorageFileName(), getStorageName());
+      return new RequestContextLocator(request);
     }
 
     public ContentFragment getFragment() {
       return new ContentFragment() {
         @Override
         public Long getOffset() {
-          return HttpGetContentAccessCommand.this.getOffset();
+          return HttpReadContentCommand.this.getOffset();
         }
 
         @Override
         public Long getLength() {
-          return HttpGetContentAccessCommand.this.getLength();
+          return HttpReadContentCommand.this.getLength();
         }
       };
-    }
-
-    private String getStorageName() {
-      return request.getParameter(STORAGE_NAME_PARAM);
-    }
-
-    private String getStorageFileName() {
-      return request.getParameter(STORAGE_FILE_NAME_PARAM);
     }
 
     private Long getOffset() {
@@ -114,6 +150,30 @@ public class HttpRemoteContentAccessServlet extends HttpServlet {
 
     private Long getLength() {
       return Long.parseUnsignedLong(request.getParameter(LENGTH_PARAM));
+    }
+  }
+
+  @RequiredArgsConstructor
+  private static class HttpWriteContentCommand {
+    private final HttpServletRequest request;
+    
+    public ContentLocator getContextLocator() {
+      return new RequestContextLocator(request);
+    }
+  }
+
+  @RequiredArgsConstructor
+  private static class RequestContextLocator implements ContentLocator {
+    private final HttpServletRequest request;
+
+    @Override
+    public String getStorageName() {
+      return request.getParameter(STORAGE_NAME_PARAM);
+    }
+
+    @Override
+    public String getStorageFileName() {
+      return request.getParameter(STORAGE_FILE_NAME_PARAM);
     }
   }
 }
--- src/main/java/io/bcs/fileserver/infrastructure/api/internal/WsContentAccessService.java
+++ src/main/java/io/bcs/fileserver/infrastructure/api/internal/WsContentAccessService.java
@@ -3,10 +3,13 @@ package io.bcs.fileserver.infrastructure.api.internal;
 import io.bce.logging.ApplicationLogger;
 import io.bce.logging.Loggers;
 import io.bcs.common.errors.SoapFault;
-import io.bcs.fileserver.api.services.internal.GetDirectContentUrlFault;
+import io.bcs.fileserver.api.services.internal.GetContentReadUrlFault;
+import io.bcs.fileserver.api.services.internal.GetContentWriteUrlFault;
 import io.bcs.fileserver.api.services.internal.WSContentAccess;
-import io.bcs.fileserver.api.types.internal.GetDirectContentUrlRqType;
-import io.bcs.fileserver.api.types.internal.GetDirectContentUrlRsType;
+import io.bcs.fileserver.api.types.internal.GetDirectContentReadUrlRqType;
+import io.bcs.fileserver.api.types.internal.GetDirectContentReadUrlRsType;
+import io.bcs.fileserver.api.types.internal.GetDirectContentWriteUrlRqType;
+import io.bcs.fileserver.api.types.internal.GetDirectContentWriteUrlRsType;
 import io.bcs.fileserver.domain.Constants;
 import io.bcs.fileserver.infrastructure.FileServerConfigurationProperties;
 import javax.inject.Inject;
@@ -23,43 +26,65 @@ import lombok.RequiredArgsConstructor;
     serviceName = "WSContentAccess",
     endpointInterface = "io.bcs.fileserver.api.services.internal.WSContentAccess")
 public class WsContentAccessService implements WSContentAccess {
-  private static final String URL_PATH_FORMAT =
+  private static final String READ_URL_PATH_FORMAT =
       "%s/private/access/direct/content?storageName=%s&storageFileName=%s&offset=%s&length=%s";
+  private static final String WRITE_URL_PATH_FORMAT =
+      "%s/private/access/direct/content?storageName=%s&storageFileName=%s";
+
   ApplicationLogger logger = Loggers.applicationLogger(WsContentAccessService.class);
 
   @Inject
   private FileServerConfigurationProperties configurationProperties;
 
   @Override
-  public GetDirectContentUrlRsType getDirectContentUrl(GetDirectContentUrlRqType request)
-      throws GetDirectContentUrlFault {
+  public GetDirectContentReadUrlRsType getContentReadUrl(GetDirectContentReadUrlRqType request)
+      throws GetContentReadUrlFault {
     try {
-      GetDirectContentUrlRsType response = new GetDirectContentUrlRsType();
-      DirectContentUrlCreator urlCreator = new DirectContentUrlCreator(request);
+      GetDirectContentReadUrlRsType response = new GetDirectContentReadUrlRsType();
+      ReadContentUrlCreator urlCreator = new ReadContentUrlCreator(request);
       response.setBoundedContext(Constants.CONTEXT.toString());
-      response.setContentRef(urlCreator.createContentRef());
+      response.setContentRef(urlCreator.createContentReadRef());
       return response;
     } catch (Exception error) {
       SoapFault faultInfo = SoapFault.createFor(error);
-      throw new GetDirectContentUrlFault(faultInfo.getMessage(), faultInfo);
+      throw new GetContentReadUrlFault(faultInfo.getMessage(), faultInfo);
+    }
+  }
+
+  @Override
+  public GetDirectContentWriteUrlRsType getContentWriteUrl(
+      GetDirectContentWriteUrlRqType parameters) throws GetContentWriteUrlFault {
+    try {
+      GetDirectContentWriteUrlRsType response = new GetDirectContentWriteUrlRsType();
+      WriteContentUrlCreator urlCreator = new WriteContentUrlCreator(parameters);
+      response.setBoundedContext(Constants.CONTEXT.toString());
+      response.setContentRef(urlCreator.createContentWriteRef());
+      return response;
+    } catch (Exception error) {
+      SoapFault faultInfo = SoapFault.createFor(error);
+      throw new GetContentWriteUrlFault(faultInfo.getMessage(), faultInfo);
     }
   }
 
   @RequiredArgsConstructor
-  private class DirectContentUrlCreator {
-    private final GetDirectContentUrlRqType request;
+  private class ReadContentUrlCreator {
+    private final GetDirectContentReadUrlRqType request;
 
-    public String createContentRef() {
-      return String.format(URL_PATH_FORMAT, getContextRoot(), getStorageFileName(), getOffset(),
-          getLength());
+    public String createContentReadRef() {
+      return String.format(READ_URL_PATH_FORMAT, getContextRoot(), getStorageName(),
+          getStorageFileName(), getOffset(), getLength());
     }
 
     private String getContextRoot() {
       return configurationProperties.getPrivateBaseUrlAddress();
     }
 
+    private String getStorageName() {
+      return request.getLocator().getStorageName();
+    }
+
     private String getStorageFileName() {
-      return request.getStorageFileName();
+      return request.getLocator().getStorageFileName();
     }
 
     private Long getOffset() {
@@ -68,6 +93,28 @@ public class WsContentAccessService implements WSContentAccess {
 
     private Long getLength() {
       return request.getFragment().getLength();
+    }
+  }
+
+  @RequiredArgsConstructor
+  private class WriteContentUrlCreator {
+    private final GetDirectContentWriteUrlRqType request;
+
+    public String createContentWriteRef() {
+      return String.format(WRITE_URL_PATH_FORMAT, getContextRoot(), getStorageName(),
+          getStorageFileName());
+    }
+
+    private String getContextRoot() {
+      return configurationProperties.getPrivateBaseUrlAddress();
+    }
+
+    private String getStorageName() {
+      return request.getLocator().getStorageName();
+    }
+
+    private String getStorageFileName() {
+      return request.getLocator().getStorageFileName();
     }
   }
 }
